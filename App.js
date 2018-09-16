10b90b1e7562a2624d1033b1a5a88652ed11105c
Cyril COLINET <cyril.colinet@epitech.eu>
2018-09-16 16:11:24 +0200

    Splash

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
+/**
+ * Index app with spash screen
+ * @author Face06
+ */
 import React, { Component } from 'react';
-import { Examples } from '@shoutem/ui';
+import { StyleSheet, View, Image } from 'react-native';
+import { ImageBackground } from '@shoutem/ui';
 
-export default class App extends Component<{}> {
+export default class App extends Component {
     render() {
         return (
-            <Examples />
+            <View style={styles.container}>
+                <ImageBackground
+                    styleName="large-portrait"
+                    source={{ uri: 'http://genchi.info/images/recording-studio-wallpaper-15.jpg' }}>
+                    <Image source={{ uri: 'https://campus-media.face06.com/RUN-Logo.png' }} style={{width:200, height: 200}} />
+                </ImageBackground>
+            </View>
         );
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
